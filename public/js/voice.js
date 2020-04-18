122ea4eab36407b0ab2a4b7ba45d28cbea95c596
David Coons <dlc9005@gmail.com>
2020-04-17 19:34:04 -0700

    added word capture functionality and save capabilities

--- public/js/voice.js
+++ public/js/voice.js
@@ -8,44 +8,67 @@ if (SpeechRecognition) {
     searchForm.insertAdjacentHTML("beforeend", '<button type="button" id="input"><i class="fas fa-microphone"></i></button>')
     const micBtn = searchForm.querySelector("button");
     const micIcon = micBtn.querySelector("i");
-  
+
     const recognition = new SpeechRecognition();
     recognition.continuous = true;
 
     micBtn.addEventListener("click", micBtnClick)
     function micBtnClick() {
-       
-       if (micIcon.classList.contains("fa-microphone")) { //Start speech recognition
-        recognition.start();
+
+        if (micIcon.classList.contains("fa-microphone")) { //Start speech recognition
+            recognition.start();
         }
-       else { //Stop Speech recognition
-        recognition.stop()
-    }
-}
-
-recognition.onstart = function startSpeechRecogniton (){
-    micIcon.classList.remove("fa-microphone")
-    micIcon.classList.add("fa-microphone-slash")
-    searchFormInput.focus();
-    console.log("Speech recognition active.")
-}
-recognition.onend = function endSpeechRecognition() {
-    micIcon.classList.remove("fa-microphone-slash");
-    micIcon.classList.add("fa-microphone")
-    searchFormInput.focus();
-    console.log("Speech recognition is not active.")
+        else { //Stop Speech recognition
+            recognition.stop()
+        }
     }
 
+    recognition.onstart = function startSpeechRecogniton() {
+        micIcon.classList.remove("fa-microphone")
+        micIcon.classList.add("fa-microphone-slash")
+        searchFormInput.focus();
+        console.log("Speech recognition active.")
+    }
+    recognition.onend = function endSpeechRecognition() {
+        micIcon.classList.remove("fa-microphone-slash");
+        micIcon.classList.add("fa-microphone")
+        searchFormInput.focus();
+        console.log("Speech recognition is not active.")
+    }
+
+    // function alertFunction() {
+    // alert("working")
+    // }
 
     recognition.onresult = function (event) {
         const currentResultIndex = event.resultIndex
         console.log(event.resultIndex)
         const textArea = document.querySelector("#textarea")
+        const textAreaTwo = document.querySelector("#textareatwo")
+        const save = document.querySelector("#save")
+        const textResults = document.querySelector("#textresults")
+        const results = document.querySelector("#results")
+
         const transcript = event.results[currentResultIndex][0].transcript;
-        searchFormInput.value  = transcript;
+        searchFormInput.value = transcript;
         textArea.innerHTML = transcript;
         console.log(transcript)
 
+
+        save.addEventListener("click", function () {
+            textAreaTwo.innerHTML = textArea.innerHTML
+        })
+
+        results.addEventListener("click", function () {
+            const resultsText = textArea.innerHTML
+            const grabText = resultsText.match(/David/g)
+            // const grabTextValue = grabText.value
+            console.log(grabText[0])
+            if (grabText[0] === "David") {
+               const counter = grabText.length
+                 textResults.innerHTML = `You said ${grabText[0]} ${counter} times! Let's work on that a bit more shall we?`
+            }
+        })
 
 
         if (transcript.toLowerCase().trim() === "stop recording") {
@@ -65,6 +88,11 @@ recognition.onend = function endSpeechRecognition() {
                 searchFormInput.value = ""
                 textArea.innerHTML = ""
             }
+            // else if (transcript.toLowerCase().trim() === "save") {
+            //     // alert("working")
+            //     // const grabText = document.querySelector
+            //     textAreaTwo.innerHTML = textArea.innerHTML
+            // }
             else {
                 searchFormInput.value = transcript;
             }
@@ -82,7 +110,7 @@ recognition.onend = function endSpeechRecognition() {
         // setTimeout(() => {
         //     searchForm.submit();
         // }, 750)
-}
+    }
 
 
 }
